5c0165bbf10be2cf488b18dc9b411940a3d8ef57
ritssoft <63286559+ritssoft@users.noreply.github.com>
2022-03-23 11:35:54 +0530

    8

---
+++ Terraform-AWS/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.6.0"
+    }
+  }
+  backend "s3" {
+    bucket = "rits-s3"
+    region = "ap-south-1"
+    key = "aws/terraform.tfstate"
+  }
+}
